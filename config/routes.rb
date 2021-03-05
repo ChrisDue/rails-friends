885af85845e1e5dc8080bd6371ac8b53d285e10b
Christopher Dührkop <christopher.duehrkop@jobleads.com>
2021-03-05 12:11:30 +0100

    install devise for user management

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users # handles the whole user gem "devise"
   resources :friends # handles all the freaking CRUD and table routes in one!
   # get 'home/index'
   get 'home/about'
